8a0a82e3df4188fb5595631da83545d61e585535
Kal <sneakyazshiite@gmail.com>
2021-04-06 19:43:47 -0400

    Full XAML, empty events for .cs file

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RDR2PhotoConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnDefaultPathClicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnCustomPathClicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnSetDirectoryClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
