e789fc7632917dcd8dbe55cd5e2b31295f513a4d
Kal <sneakyazwhat@gmail.com>
2021-04-11 20:58:32 -0400

    Getting path for MyDocuments in a cleaner fashion to account for it being on different hard drive

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Windows;
 using System.Windows.Input;
@@ -13,7 +14,8 @@ namespace RDR2PhotoConverter
     /// </summary>
     public partial class MainWindow : Window
     {
-        private static string userName = Environment.UserName;
+        private static string userName;
+
         private static string defaultDirPRDR;
         private static string customDirPRDR;
         private static string activeDir;
@@ -28,22 +30,13 @@ namespace RDR2PhotoConverter
         {
             InitializeComponent();
 
-            string[] fullDir = Directory.GetDirectories($"C:\\Users\\{userName}\\Documents\\Rockstar Games\\Red Dead Redemption 2\\Profiles");
-            defaultDirPRDR = fullDir[0];
+            userName = Environment.UserName;
 
-            convertedFilesDir = $"C:\\Users\\{userName}\\Pictures\\RDR2 Photos";
-            backupDirPRDR = $"C:\\Users\\{userName}\\Pictures\\RDR2 Photos\\prdr backups";
-
-            if (!Directory.Exists(convertedFilesDir))
-            {
-                Directory.CreateDirectory(convertedFilesDir);
-            }
-            else if (!Directory.Exists(backupDirPRDR))
-            {
-                Directory.CreateDirectory(backupDirPRDR);
-            }
+            GetDefaultDirectory();
+            SetDirectories();
 
             dirInputTextBox.Text = defaultDirPRDR;
+
         }
 
         #region ClickEvents
@@ -63,12 +56,13 @@ namespace RDR2PhotoConverter
 
         private void OnSetDirectoryClick(object sender, RoutedEventArgs e)
         {
+            MessageBox.Show("set directory clicked test yeah this this is a long long boi message to see what this looks lijke i dont know why my app doesnt work pls microsoft fix me ty");
 
         }
 
         private void OnConvertFilesClick(object sender, RoutedEventArgs e)
         {
-
+            //TODO - MessageBox.Show() at the end of converting process to notify user it is completed! :)
         }
 
         private void OnMyTwitterClick(object sender, RoutedEventArgs e)
@@ -83,6 +77,39 @@ namespace RDR2PhotoConverter
 
         #endregion
 
+        #region Getters
+        private void GetDefaultDirectory() //Trying to get default directory, small % of users may have an issue with their USER not matching their USERNAME
+        {
 
+            string[] fulldir = Directory.GetDirectories($"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\\Rockstar Games\\Red Dead Redemption 2\\Profiles");
+            defaultDirPRDR = fulldir[0];
+
+            Debug.WriteLine(defaultDirPRDR);
+
+        }
+
+        #endregion
+
+        #region Setters
+        private void SetDirectories()
+        {
+            ////This stuff runs only after the default directory issue is resolved and set (in case it needs to be changed from environment.username)
+            //MessageBox.Show("Congrats, this program doesn't break on your PC. The rest of the application is disabled right now as this is just a testing build. PLEASE let me know it worked for you!");
+
+            ////Use this to find the pictures folder and then create the new subdirectories based on that
+            //Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            //convertedFilesDir = $"C:\\Users\\{userName}\\Pictures\\RDR2 Photos";
+            //backupDirPRDR = $"C:\\Users\\{userName}\\Pictures\\RDR2 Photos\\prdr backups";
+
+            //if (!Directory.Exists(convertedFilesDir))
+            //{
+            //    Directory.CreateDirectory(convertedFilesDir);
+            //}
+            //else if (!Directory.Exists(backupDirPRDR))
+            //{
+            //    Directory.CreateDirectory(backupDirPRDR);
+            //}
+        }
+        #endregion
     }
 }
