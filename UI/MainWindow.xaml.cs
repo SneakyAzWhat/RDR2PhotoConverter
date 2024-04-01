3c0ef869d9c1278dd9b4ab40b343d4e75ddb53ac
Benjamin Joseph <benjamin.joseph212620@gmail.com>
2024-04-01 22:40:30 +0530

    basic layout

---
+++ UI/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using RDR2PhotoConverter.Frames;
+using System.Windows;
+using System.Windows.Input;
+
+namespace RDR2PhotoConverter.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DirectorySelect DirectorySelectPage { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            DirectorySelectPage = new DirectorySelect();
+            ParentContainer.Content = DirectorySelectPage;
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TitleBar_Drag(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+    }
+}
