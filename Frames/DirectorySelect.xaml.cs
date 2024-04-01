8bbcb2faacc6d7e2ca42ef4358fce8d2c63ef40e
Benjamin Joseph <benjamin.joseph212620@gmail.com>
2024-04-01 22:40:44 +0530

    directory select frame

---
+++ Frames/DirectorySelect.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RDR2PhotoConverter.Frames
+{
+    /// <summary>
+    /// Interaction logic for DirectorySelect.xaml
+    /// </summary>
+    public partial class DirectorySelect : Page
+    {
+        public DirectorySelect()
+        {
+            InitializeComponent();
+        }
+    }
+}
