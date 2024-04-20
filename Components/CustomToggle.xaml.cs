89272fe578b6406ccb54619900a0f67d95a5aff3
Benjamin Joseph <benjamin.joseph212620@gmail.com>
2024-04-20 16:50:28 +0530

    added custom toggle buttons

---
+++ Components/CustomToggle.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace RDR2PhotoConverter.Components
+{
+    /// <summary>
+    /// Interaction logic for CustomToggle.xaml
+    /// </summary>
+    public partial class CustomToggle : UserControl
+    {
+        private bool _isChecked = false;
+
+        public bool IsChecked
+        {
+            get { return _isChecked; }
+            set
+            {
+                _isChecked = value;
+                UpdateVisualState();
+            }
+        }
+
+        public CustomToggle()
+        {
+            InitializeComponent();
+            UpdateVisualState();
+        }
+
+        private void ToggleGrid_MouseLeftButtonDown(object sender, RoutedEventArgs e)
+        {
+            IsChecked = !IsChecked;
+        }
+
+        private void UpdateVisualState()
+        {
+            if (IsChecked)
+            {
+                var animation = new DoubleAnimation(-20, 20, TimeSpan.FromSeconds(0.2));
+                NoText.Visibility = Visibility.Collapsed;
+                YesText.Visibility = Visibility.Visible;
+                ToggleGrid.Background = new SolidColorBrush(System.Windows.Media.Color.FromRgb(255, 193, 0));
+                ToggleTransform.BeginAnimation(TranslateTransform.XProperty, animation);
+            }
+            else
+            {
+                var animation = new DoubleAnimation(20, -20, TimeSpan.FromSeconds(0.2));
+                NoText.Visibility = Visibility.Visible;
+                YesText.Visibility = Visibility.Collapsed;
+                ToggleGrid.Background = new SolidColorBrush(System.Windows.Media.Color.FromRgb(211, 211, 211));
+                ToggleTransform.BeginAnimation(TranslateTransform.XProperty, animation);
+            }
+        }
+    }
+}
