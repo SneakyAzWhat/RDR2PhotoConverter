288cbe08d7c36d0f9bad871345e11a5b108556b0
Kal <sneakyazwhat@gmail.com>
2026-06-13 15:21:13 -0400

    Added application wide exception handling, intended to catch exceptions that occur outside of current try/catch blocks

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,5 +13,26 @@ namespace RDR2PhotoConverter
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+            AppDomain.CurrentDomain.UnhandledException += App_UnhandledException;
+        }
+
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            //TODO
+            //LOG information to a file, this is a general unhandled exception that wasn't handled within my normal try/catch blocks
+            MessageBox.Show($"An unexpected error occurred and the application needs to close.\n\nRAW: {e.Exception.Message}");
+            e.Handled = true;
+        }
+
+        private void App_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception ex = e.ExceptionObject as Exception;
+            MessageBox.Show($"A fatal error occurred.\n\nRAW: {ex?.Message}");
+        }
     }
 }
